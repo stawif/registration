a28e06b7b1c7b9e729a13559cc2e3d582bb5882a
Riddhesh Jangid <riddhesh710@gmail.com>
2020-02-19 17:32:06 +0530

    Adding unique fields

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
 
 class Owner(models.Model):
     """
     Owner is the actor for which this application is build    
     """
-    name = models.CharField(max_length=30,blank=False)
-    username = models.CharField(max_length=30,blank=False)
+    name = models.CharField(max_length=30,blank=False,unique=True)
+    username = models.CharField(max_length=30,blank=False,unique=True)
     password = models.CharField(max_length=30,blank=False)
     contact = models.IntegerField(blank=False)
 
@@ -17,7 +19,7 @@ class Machine(models.Model):
     Machines are assets of owner which are used in Work
     """
     owner = models.ForeignKey(Owner,on_delete=models.CASCADE)    
-    name = models.CharField(max_length=30,blank=False)
+    name = models.CharField(max_length=30,blank=False,unique=True)
   
     def __str__(self):
         return self.name    
@@ -27,7 +29,7 @@ class Vehicle(models.Model):
     Vehicles are assets of owner which are used in Work
     """
     owner = models.ForeignKey(Owner,on_delete=models.CASCADE)    
-    name = models.CharField(max_length=30,blank=False)
+    name = models.CharField(max_length=30,blank=False,unique=True)
 
     def __str__(self):
         return self.name    
@@ -54,6 +56,13 @@ class Store(models.Model):
 
     def __str__(self):
         return self.name    
+
+@receiver(pre_save, sender=Store)
+def check_item_availability(sender,instance,**kwarge):
+    allItems = sender.objects.filter(owner=instance.owner)
+    for item in allItems:
+        print(item.name)
+
 
 type_of_party = (
     ('Machine_work','Machine_work'),
