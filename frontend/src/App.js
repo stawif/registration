c2f52f1869d90e2386ab19bca9e2e497b6684ace
Riddhesh Jangid <riddhesh710@gmail.com>
2020-02-21 00:58:17 +0530

    Wroking on input Field

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './machine.css';
-//import './App.css';
-//import MachineRegistration from './components/registration/MachineRegistration';
-//import BasicValidation from './components/registration/BasicValidation';
-//import PartyReg from './components/registration/PartyReg';
-//import PartyDisplay from './components/displaydata/PartyDisplay';
-//import VehicleReg from './components/registration/VehicleReg';
+import MachineRegistration from './components/registration/MachineRegistration';
+import inputField from './inputField';
 
-class App extends React.Component {
-	render() {
+function App(){
 		return (
 			<div>
 				<div id="container" className="row">
 					<div id="controller" className="col-sm-3">
 						<center> <p className="headingDashboard">DASHBOARD</p> </center>
+						<inputField />
 					</div>
 					<div className="col-sm-9">
-					
 						<nav className="navbar navbar-expand-lg navbar-light bg-light">
 							<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 								<span className="navbar-toggler-icon"></span>
@@ -25,36 +20,36 @@ class App extends React.Component {
 							<div className="collapse navbar-collapse" id="navbarSupportedContent">
 								<ul className="navbar-nav mr-auto">
 									<li className="nav-item active">
-										<a className="nav-link" href="#">Logout<span className="sr-only">(current)</span></a>
+										<a className="nav-link" href="">Logout<span className="sr-only">(current)</span></a>
 									</li>
 									<li className="nav-item">
-										<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<a className="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 										Registration
 										</a>
 										<div className="dropdown-menu" aria-labelledby="navbarDropdown">
-											<a className="dropdown-item" href="#">Machine</a>
-											<a className="dropdown-item" href="#">Vehicle</a>
-											<a className="dropdown-item" href="#">Party</a>
+											<a className="dropdown-item" href="">Machine</a>
+											<a className="dropdown-item" href="">Vehicle</a>
+											<a className="dropdown-item" href="">Party</a>
 										</div>
 									</li>
 									<li className="nav-item dropdown">
-										<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<a className="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 										Entry
 										</a>
 										<div className="dropdown-menu" aria-labelledby="navbarDropdown">
-											<a className="dropdown-item" href="#">Machine Work</a>
-											<a className="dropdown-item" href="#">Vehicle Work</a>
-											<a className="dropdown-item" href="#">Daily Work</a>
+											<a className="dropdown-item" href="">Machine Work</a>
+											<a className="dropdown-item" href="">Vehicle Work</a>
+											<a className="dropdown-item" href="">Daily Work</a>
 										</div>
 									</li>
 									<li className="nav-item">
-										<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<a className="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 										Display
 										</a>
 										<div className="dropdown-menu" aria-labelledby="navbarDropdown">
-											<a className="dropdown-item" href="#">Account</a>
-											<a className="dropdown-item" href="#">Party status</a>
-											<a className="dropdown-item" href="#">Store status</a>
+											<a className="dropdown-item" href="">Account</a>
+											<a className="dropdown-item" href="">Party status</a>
+											<a className="dropdown-item" href="">Store status</a>
 										</div>
 									</li>
 								</ul>
@@ -78,7 +73,6 @@ class App extends React.Component {
 				</div>
 			</div>
 		);
-	}
 }
 
 
---
+++ frontend/src/inputField.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function inputField()
+{
+    return (
+        <div>
+            <h3>It works</h3>
+        </div>
+    );
+}
+
+
+
