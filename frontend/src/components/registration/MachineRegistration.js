3d64d99f86ac4d3ec1069f52106ff4ce0e359a75
Riddhesh Jangid <riddhesh710@gmail.com>
2020-02-21 20:20:08 +0530

    Rebuild MachineRegistration component from class for solving issue of loop api calling

--- frontend/src/components/registration/MachineRegistration.js
+++ frontend/src/components/registration/MachineRegistration.js
@@ -1,56 +1,97 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useForm } from "react-hook-form";
-import InputField from '../modular/InputField';
+import qs from 'qs';
+//import { useForm } from "react-hook-form";
+//import InputField from '../modular/InputField';
 
-export default function MachineRegistration() {
-  const [machineName, setMachineName] = useState("");
 
-  const { register, errors, handleSubmit, reset } = useForm();
+export default class MachineRegistration extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state={
+      machienName: "",
+      errorMessage: "",
+      machineList: {},
+      buttonStatus: {
+        visibility: 'visible'
+      }
+    }
+
+    this.state.fetchProduct = async () =>{
+      console.log("Fetch calls");
+      const responseMachineList = await fetch("http://127.0.0.1:8000/list-of-machines/");
+      const jsonMachineList = await responseMachineList.json();
+      this.state.machineList = jsonMachineList;
+    }
+    this.state.fetchProduct(); 
+
+    this.state.checkMachine = () => {
+      console.log("Come in check");
+      try {
+        this.setState({
+             errorMessage :"",
+             buttonStatus: {
+              visibility: 'visible'           
+             }
+            });
+        const showList = (item, index) => {
+            if (this.state.machienName.localeCompare(item.name) == 0){
+              this.setState({
+                 errorMessage :"* This machine name is already exist!!!",
+                 buttonStatus: {
+                  visibility: 'hidden'                 
+                 }
+              });
+            }
+            else{}
+        };
+        this.state.machineList.forEach(showList);
+      } 
+      catch (err) {}
+    }
+
+    this.state.onSubmit =(e) => {
+        axios.post('http://127.0.0.1:8000/machine-registration/', 
+        {
+          name: this.state.machienName
+        }
+        ).then(res => {
+          this.state.fetchProduct();
+        }
+        ).catch(error => {
+          alert( error.response.request._response )
+        });
+      e.target.reset();
+      e.preventDefault();
+    };
   
-  const onSubmit = (data, e) => {
-    alert("machine registered successfully"+machineName);
-    axios.post(`http://127.0.0.1:8000/machine-registration/`, {
-        name: machineName
-      })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-        //setMachineList(res);
-      });
-    e.target.reset();
-  };
-  console.log(errors);
-
-  return (
-							<form className="form-container form-group" onSubmit={handleSubmit(onSubmit)}>
-							<p className="headingViewPart">Machine Registration</p>
-							<div className="pt-5">
-                <InputField
-                  type={"text"}
-                  name={"machineName"} 
-                  placeholder={"Machine name"}
-                  machineName = {machineName}
-                  onChange = { e => setMachineName(e.target.value) } 
-                />
-							</div>
-              <br/>
-							<button type="submit" className="btn btn-outline-dark">Save</button>
-							</form>      
-  );
+  }
+ 
+  render(){
+    return (
+		<form className="form-container form-group" onSubmit={ e => this.state.onSubmit(e) }>
+    <p className="headingViewPart">Machine Registration</p>
+		<div className="pt-5">
+        <input 
+        type="text" 
+        className="mb-2" 
+        name="machineName" 
+        placeholder="Machine Name" 
+        autocomplete="off"
+        maxlength = "30"
+        minLength = "5"
+        onChange={
+          e => {
+          this.state.machienName = e.target.value;
+          this.state.checkMachine();
+        }
+        } 
+        />
+    </div>    
+    <p>{this.state.errorMessage}</p>     
+    <button type="submit" className="btn btn-outline-dark" style={this.state.buttonStatus} >Save</button>
+    </form>  
+    );
+  }
 }
-
-/*
-                <input 
-                    type="text" 
-                    className="mb-2" 
-                    name="machineName" 
-                    ref={register({
-                      required: true,
-                      maxLength: 30,
-                      minLength: { value: 5, message: "Min length is 5" }
-                    })}
-                    placeholder="Machine Name" 
-                    onChange={e => userInput(e.target.value)} 
-                />
-*/
